a8655846886ff28952b3f790eb39619c02141940
Stefan Müller <St.Mueller@hu-berlin.de>
2025-03-20 22:34:06 +0100

    new langscibook.cls, changed for lualatex, new memoize (from texlive 2024 load path)

--- Grammatiken/Kapitel-06/.trale_user_cmd_PackMuck.local_79316.pl
+++
@@ -1,2 +0,0 @@
-:- compile_gram('theory.pl').
-:- delete_file('/Users/stefan/Documents/Dienstlich/Buecher/HPSG-Lehrbuch-LangSci/Grammatiken/Kapitel-06/./.trale_user_cmd_PackMuck.local_79316.pl',[ignore]).
